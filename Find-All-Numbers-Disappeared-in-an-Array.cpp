ffa7586df7f2a83167e932f02f2f9b28d07dc98d
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2025-11-27 01:41:35 +0600

    [448] [Time Beats: 49.72%] [Memory Beats: 75.48%] - LeetPush

--- Find-All-Numbers-Disappeared-in-an-Array.cpp
+++ Find-All-Numbers-Disappeared-in-an-Array.cpp
@@ -1,17 +1,17 @@
 1class Solution {
 2public:
 3    vector<int> findDisappearedNumbers(vector<int>& nums) {
-4        unordered_set<int> k;
-5        for(auto a:nums)
+4        vector<int> res;
+5        for(int i=0;i<nums.size();i++)
 6        {
-7            k.insert(a);
-8        }
-9        vector<int> res;
-10        for(int i=1;i<=nums.size();i++)
+7            const int idx = abs(nums[i])-1;
+8            nums[idx] = -abs(nums[idx]);
+9        }
+10        for(int i=0;i<nums.size();i++)
 11        {
-12            if(k.find(i)==k.end())
+12            if(nums[i]>0)
 13            {
-14                res.push_back(i);
+14                res.push_back(i+1);
 15            }
 16        }
 17        return res;
