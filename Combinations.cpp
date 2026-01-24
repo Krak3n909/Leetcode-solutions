f532abf8e83792884f2a7ff7e1c9d8edb550ff90
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-01-24 13:06:03 +0600

    [77] [Time Beats: 91.05%] [Memory Beats: 83.37%] - LeetPush

--- Combinations.cpp
+++ Combinations.cpp
@@ -1,23 +1,25 @@
 1class Solution {
 2public:
-3    void trav(vector<vector<int>>& res, int& n, int& k, int idx,
-4              vector<int> tmp) {
-5
-6        tmp.push_back(idx);
-7        if (tmp.size() == k) {
-8            res.push_back(tmp);
-9            return;
-10        }
-11        for (int i = idx; i < n; i++) {
-12            trav(res, n, k, i + 1, tmp);
-13        }
-14    }
-15
-16    vector<vector<int>> combine(int n, int k) {
-17        vector<vector<int>> res;
-18        for (int i = 1; i <= n; i++) {
-19            trav(res, n, k, i, {});
-20        }
-21        return res;
-22    }
-23};
+3
+4    
+5    vector<int> tmp;
+6    void trav(vector<vector<int>>& res, int& n, int& k, int idx) {
+7
+8        if (tmp.size() == k) {
+9            res.push_back(tmp);
+10            return;
+11        }
+12        for (int i = idx; i <= n; i++) {
+13            tmp.push_back(i);
+14            trav(res, n, k, i + 1);
+15            tmp.pop_back();
+16        }
+17    }
+18
+19    vector<vector<int>> combine(int n, int k) {
+20        vector<vector<int>> res;
+21        trav(res, n, k, 1);
+22
+23        return res;
+24    }
+25};
