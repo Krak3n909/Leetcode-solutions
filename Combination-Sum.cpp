48702d009df44f42be318de181e90d862cf046f5
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-01-25 11:26:42 +0600

    [39] [Time Beats: 100.00%] [Memory Beats: 92.31%] - LeetPush

--- Combination-Sum.cpp
+++ Combination-Sum.cpp
@@ -1,30 +1,27 @@
 1class Solution {
-2private:
-3    void mc(vector<int>& candidates,int target,int idx,vector<int>& cmb, int total,vector<vector<int>>& res)
-4    {
-5        if(total==target)
-6        {
-7            res.push_back(cmb);
+2public:
+3
+4    void trav(vector<vector<int>>& k, vector<int>& candidates, int& target, int idx, vector<int>& tmp, int sum)
+5    {
+6        if(sum>target || idx>=candidates.size())
+7        {
 8            return;
 9        }
-10
-11        if(total>target || idx>=candidates.size())
-12        {
+10        if(sum==target )
+11        {
+12            k.push_back(tmp);
 13            return;
 14        }
-15        cmb.push_back(candidates[idx]);
-16        mc(candidates,target,idx,cmb,total+candidates[idx],res);
-17        cmb.pop_back();
-18        mc(candidates,target,idx+1,cmb,total,res);
-19
+15        tmp.push_back(candidates[idx]);
+16        trav(k, candidates, target, idx, tmp, sum+ candidates[idx]);
+17        tmp.pop_back();
+18        trav(k, candidates, target, idx+1, tmp, sum);
+19    }
 20
-21    }
-22public:
-23    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-24        vector<vector<int>> res;
-25        vector<int> cmb;
-26        mc(candidates,target,0,cmb,0,res);
-27        return res;
-28    }
-29
-30};
+21    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+22        vector<vector<int>> k;
+23        vector<int> tmp;
+24        trav(k, candidates, target, 0, tmp, 0);
+25        return k;
+26    }
+27};
