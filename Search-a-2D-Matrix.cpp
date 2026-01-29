327bfe1c8dc69f397d4436e783d8c09b64d625d6
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-01-29 12:43:30 +0600

    [74] [Time Beats: 100.00%] [Memory Beats: 97.74%] - LeetPush

--- Search-a-2D-Matrix.cpp
+++ Search-a-2D-Matrix.cpp
@@ -16,42 +16,41 @@
 16            }
 17            else if(matrix[targetRow][col-1] < target)
 18            {
-19                startRow = targetRow + 1;
-20            }
-21            else if(matrix[targetRow][0] > target)
-22            {
-23                endRow = targetRow - 1;
+19                if(targetRow == row - 1)
+20                {
+21                    return false;
+22                }
+23                startRow = targetRow + 1;
 24            }
-25            else
+25            else if(matrix[targetRow][0] > target)
 26            {
-27                targetRow = -1;
-28                break;
-29            }
-30        }
-31        if(targetRow == -1)
-32        {
-33            return false;
-34        }
-35        int l = 0;
-36        int r = col - 1;
-37        while(l <= r)
-38        {
-39            int mid = (l + r) / 2;
-40            
-41            cout<<0<<mid<<endl;
-42            if(matrix[targetRow][mid] == target)
-43            {
-44                return true;
-45            }
-46            else if( matrix[targetRow][mid] > target )
-47            {
-48                r = mid - 1;
-49            }
-50            else 
-51            {
-52                l = mid + 1; 
-53            }
-54        }
-55        return false;
-56    }
-57};
+27                if(targetRow == 0)
+28                {
+29                    return false;
+30                }
+31                endRow = targetRow - 1;
+32            }
+33        }
+34        int l = 0;
+35        int r = col - 1;
+36        while(l <= r)
+37        {
+38            int mid = (l + r) / 2;
+39            
+40            cout<<0<<mid<<endl;
+41            if(matrix[targetRow][mid] == target)
+42            {
+43                return true;
+44            }
+45            else if( matrix[targetRow][mid] > target )
+46            {
+47                r = mid - 1;
+48            }
+49            else 
+50            {
+51                l = mid + 1; 
+52            }
+53        }
+54        return false;
+55    }
+56};
