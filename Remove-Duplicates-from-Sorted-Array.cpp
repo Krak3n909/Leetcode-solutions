08884a81740d14f6e31812ad3bfb82addcad9d67
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-03-16 22:11:58 +0600

    [26] [Time Beats: 100.00%] [Memory Beats: 52.13%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array.cpp
+++ Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,20 +1,18 @@
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        int i=0;
-5        int j=0;
-6        while(j<nums.size() && i<nums.size())
-7        {
-8            if(nums[i]>=nums[j])
-9            {
-10                j++;
-11            }
-12            else
-13            {
-14                swap(nums[++i],nums[j]);
-15                j++;
-16            }
-17        }
-18        return i+1;
-19    }
-20};
+4        int i = 0, j = 0;
+5        while(i < nums.size() && j < nums.size())
+6        {
+7            if(nums[i] == nums[j])
+8            {
+9                j++;
+10            }
+11            else
+12            {
+13                nums[++i] = nums[j];
+14            }
+15        }
+16        return i + 1;
+17    }
+18};
