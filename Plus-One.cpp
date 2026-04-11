2cedef3292cedd2a60f5b55738af9cc382b95733
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-04-12 03:10:58 +0600

    [66] [Time Beats: 100.00%] [Memory Beats: 62.32%] - LeetPush

--- Plus-One.cpp
+++ Plus-One.cpp
@@ -1,29 +1,16 @@
 1class Solution {
 2public:
 3    vector<int> plusOne(vector<int>& digits) {
-4        int carry = 0;
-5        digits[digits.size() - 1]++;
-6        for(int i = digits.size() - 1;i >= 0;i--)
-7        {
-8            int tmp = digits[i];
-9            tmp += carry;
-10            if(tmp >= 10)
-11            {
-12                carry = 1;
-13                tmp -= 10;
-14            }
-15            else
-16            {
-17                carry = 0;
-18            }
-19            digits[i] = tmp;
-20        }
-21        if(carry)
-22        {
-23            reverse(digits.begin(), digits.end());
-24            digits.push_back(carry);
-25            reverse(digits.begin(), digits.end());
-26        }
-27        return digits;
-28    }
-29};
+4        for(int i = digits.size() - 1;i >= 0;i--)
+5        {
+6            if(digits[i] < 9)
+7            {
+8                digits[i]++;
+9                return digits;
+10            }
+11            digits[i] = 0;
+12        }
+13        digits.insert(digits.begin(), 1);
+14        return digits;
+15    }
+16};
