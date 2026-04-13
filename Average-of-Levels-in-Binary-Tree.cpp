17c4a42266b5532c89d52a7db1ba541db59e6292
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-04-14 00:04:22 +0600

    [637] [Time Beats: 100.00%] [Memory Beats: 74.91%] - LeetPush

--- Average-of-Levels-in-Binary-Tree.cpp
+++ Average-of-Levels-in-Binary-Tree.cpp
@@ -12,34 +12,31 @@
 12class Solution {
 13public:
 14
-15    void trav(TreeNode* r, int lvl, vector<double> &res, vector<int> &lvc)
+15    void chk(TreeNode* r, vector<double> &ans, vector<double> &c, int l)
 16    {
 17        if(!r)
 18        {
 19            return;
 20        }
-21        if(res.size()<lvl)
+21        if(l + 1 > ans.size())
 22        {
-23            res.push_back(r->val);
-24            lvc.push_back(1);
+23            ans.push_back(0);
+24            c.push_back(0);
 25        }
-26        else
-27        {
-28            res[lvl-1] += r->val;
-29            lvc[lvl-1] ++;
-30        }
-31        trav(r->left, lvl+1, res,lvc);
-32        trav(r->right, lvl+1, res,lvc);
-33    }
-34
-35    vector<double> averageOfLevels(TreeNode* root) {
-36        vector<double> res;
-37        vector<int> lv;
-38        trav(root, 1, res,lv);
-39        for(int i=0;i<res.size();i++)
-40        {
-41            res[i]=res[i]/lv[i];
-42        }
-43        return res;
-44    }
-45};
+26        ans[l] += r->val;
+27        c[l]++;
+28        chk(r->left, ans, c, l + 1);
+29        chk(r->right, ans, c, l + 1);
+30    }
+31
+32    vector<double> averageOfLevels(TreeNode* root) {
+33        vector<double> ans;
+34        vector<double> count;
+35        chk(root, ans, count, 0);
+36        for(int i = 0;i < ans.size();i++)
+37        {
+38            ans[i] /= count[i];
+39        }
+40        return ans;
+41    }
+42};
