61b3e8c4cb63facc29d6722d77f1b76262cf75fe
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-04-15 19:52:06 +0600

    [5] [Time Beats: 58.31%] [Memory Beats: 39.33%] - LeetPush

--- Longest-Palindromic-Substring.cpp
+++ Longest-Palindromic-Substring.cpp
@@ -3,34 +3,35 @@
 3    string longestPalindrome(string s) {
 4        int c = 0;
 5        string res = "";
-6        for(int i = 0; i < s.size();i++)
+6        for(int i = 0;i < s.size();i++)
 7        {
 8            int l = i, r = i;
-9            while( l>= 0 && r < s.size() && s[l] == s[r])
+9            while(l >= 0 && r < s.size() && s[l] == s[r])
 10            {
-11                if(c < (r - l + 1))
+11                if(c < r - l + 1)
 12                {
-13                    c = r - l + 1;
-14                    res = s.substr(l, r - l + 1);
+13                    res = s.substr(l, r - l + 1);
+14                    c = r - l + 1;
 15                }
 16                l--;
 17                r++;
 18            }
 19        }
-20        for(int i = 0; i < s.size();i++)
-21        {
-22            int l = i, r = i + 1;
-23            while( l>= 0 && r < s.size() && s[l] == s[r])
-24            {
-25                if(c < (r - l + 1))
-26                {
-27                    c = r - l + 1;
+20
+21        for(int i = 0;i < s.size();i++)
+22        {
+23            int l = i, r = i + 1;
+24            while(l >= 0 && r < s.size() && s[l] == s[r])
+25            {
+26                if(c < r - l + 1)
+27                {
 28                    res = s.substr(l, r - l + 1);
-29                }
-30                l--;
-31                r++;
-32            }
-33        }
-34        return res;
-35    }
-36};
+29                    c = r - l + 1;
+30                }
+31                l--;
+32                r++;
+33            }
+34        }
+35        return res;
+36    }
+37};
