c799064b9701e3d018bbfad9dd5cfc03f5ab2bcb
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-04-17 02:00:21 +0600

    [17] [Time Beats: 100.00%] [Memory Beats: 40.43%] - LeetPush

--- Letter-Combinations-of-a-Phone-Number.cpp
+++ Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,32 +1,35 @@
 1class Solution {
 2public:
-3    void trav(vector<string>& res, int idx, string& digits, string tmp, vector<string> &k) {
-4        if (idx == digits.size()) {
-5            res.push_back(tmp);
-6            return;
-7        }
-8
-9        int i = digits[idx] - '1';
-10        for (char a : k[i]) {
-11            string m = tmp + a;
-12            trav(res, idx + 1, digits, m, k);
-13        }
-14    }
-15
-16    vector<string> letterCombinations(string digits) {
-17
-18        vector<string> k;
-19        k.push_back("");
-20        k.push_back("abc");
-21        k.push_back("def");
-22        k.push_back("ghi");
-23        k.push_back("jkl");
-24        k.push_back("mno");
-25        k.push_back("pqrs");
-26        k.push_back("tuv");
-27        k.push_back("wxyz");
-28        vector<string> res;
-29        trav(res, 0, digits, "", k);
-30        return res;
-31    }
-32};
+3
+4    void trav(vector<string> &res, vector<string> &k, string& digits, int idx, string cur)
+5    {
+6        if(idx >= digits.size())
+7        {
+8            res.push_back(cur);
+9            return;
+10        }
+11        int target = digits[idx] - '1';
+12        for(auto a:k[target])
+13        {
+14            string tmp = cur;
+15            tmp += a;
+16            trav(res, k, digits, idx + 1, tmp);
+17        }
+18    }
+19
+20    vector<string> letterCombinations(string digits) {
+21        vector<string> k;
+22        k.push_back("");
+23        k.push_back("abc");
+24        k.push_back("def");
+25        k.push_back("ghi");
+26        k.push_back("jkl");
+27        k.push_back("mno");
+28        k.push_back("pqrs");
+29        k.push_back("tuv");
+30        k.push_back("wxyz");
+31        vector<string> res;
+32        trav(res, k, digits, 0, "");
+33        return res;
+34    }
+35};
