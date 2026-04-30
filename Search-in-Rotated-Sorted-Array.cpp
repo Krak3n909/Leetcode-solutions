8ac3a75a677da5a2eccf8855d13bec39b17acf49
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-04-30 23:43:41 +0600

    [33] [Time Beats: 100.00%] [Memory Beats: 95.79%] - LeetPush

--- Search-in-Rotated-Sorted-Array.cpp
+++ Search-in-Rotated-Sorted-Array.cpp
@@ -3,37 +3,36 @@
 3    int search(vector<int>& nums, int target) {
 4        int l = 0;
 5        int r = nums.size() - 1;
-6        while (l <= r)
+6        while(l <= r)
 7        {
-8            int mid = (l + r) / 2;
-9            cout<<mid<<endl;
-10            if(nums[mid] == target)
-11            {
-12                return mid;
-13            }
-14            else if(nums[l] <= nums[mid])
-15            {
-16                if(target >= nums[l] && nums[mid] >= target)
-17                {
-18                    r = mid - 1;
-19                }
-20                else
-21                {
-22                    l = mid + 1;
-23                }
-24            }
-25            else
-26            {
-27                if(target <= nums[r] && nums[mid] <= target)
-28                {
-29                    l = mid + 1;
-30                }
-31                else
-32                {
-33                    r = mid - 1;
-34                }
-35            }
-36        }
-37        return -1;
-38    }
-39};
+8            int mid = l + (r - l) / 2;
+9            if(nums[mid] == target)
+10            {
+11                return mid;
+12            }
+13            else if(nums[l] <= nums[mid])
+14            {
+15                if(target <= nums[mid] && target >= nums[l])
+16                {
+17                    r = mid - 1;
+18                }
+19                else
+20                {
+21                    l = mid + 1;
+22                }
+23            }
+24            else
+25            {
+26                if(target >= nums[mid] && target <= nums[r])
+27                {
+28                    l = mid + 1;
+29                }
+30                else
+31                {
+32                    r = mid - 1;
+33                }
+34            }
+35        }
+36            return -1;
+37    }
+38};
