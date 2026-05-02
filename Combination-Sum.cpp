9cb033ace540385537095b5a1a40701fcd90b2d8
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2026-05-02 12:23:40 +0600

    [39] [Time Beats: 100.00%] [Memory Beats: 91.95%] - LeetPush

--- Combination-Sum.cpp
+++ Combination-Sum.cpp
@@ -1,27 +1,25 @@
 1class Solution {
 2public:
-3
-4    void trav(vector<vector<int>>& k, vector<int>& candidates, int& target, int idx, vector<int>& tmp, int sum)
-5    {
-6        if(sum>target || idx>=candidates.size())
-7        {
-8            return;
-9        }
-10        if(sum==target )
-11        {
-12            k.push_back(tmp);
-13            return;
-14        }
-15        tmp.push_back(candidates[idx]);
-16        trav(k, candidates, target, idx, tmp, sum+ candidates[idx]);
-17        tmp.pop_back();
-18        trav(k, candidates, target, idx+1, tmp, sum);
-19    }
-20
-21    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-22        vector<vector<int>> k;
-23        vector<int> tmp;
-24        trav(k, candidates, target, 0, tmp, 0);
-25        return k;
-26    }
-27};
+3    void trav(vector<vector<int>>& ans, vector<int>& candidates, int target,
+4              vector<int> &tmp, int sum, int idx) {
+5        if (sum > target || idx >= candidates.size()) {
+6            return;
+7        }
+8        if(sum == target)
+9        {
+10            ans.push_back(tmp);
+11            return;
+12        }
+13        tmp.push_back(candidates[idx]);
+14        trav(ans, candidates, target, tmp, sum + candidates[idx], idx);
+15        tmp.pop_back();
+16        trav(ans, candidates, target, tmp, sum, idx + 1);
+17    }
+18
+19    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+20        vector<vector<int>> ans;
+21        vector<int> tmp;
+22        trav(ans, candidates, target, tmp, 0, 0);
+23        return ans;
+24    }
+25};
